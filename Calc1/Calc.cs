630efbb796da6be831748b6650187df84d287b0e
Наталия <natalia.zai4ick@yandex.ru>
2017-10-07 20:09:46 +0300

    Добавьте файлы проекта.

---
+++ Calc1/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorExample
+{
+    class Calc : Program
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Calc1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            int ans = calc.Add(10, 84);
+            Console.WriteLine("10 + 84 is (0).", ans);
+            Console.ReadLine();
+        }
+    }
+}
